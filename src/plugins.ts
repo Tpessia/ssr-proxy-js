8b80ad07f6602f78728d6da1cbc877df3985abf0
Tpessia <thiago.pessia@gmail.com>
2025-03-27 23:23:51 -0300

    Add Vite plugin event option

--- src/plugins.ts
+++ src/plugins.ts
@@ -4,12 +4,13 @@ import { SsrBuildConfig } from './types';
 type Apply = 'serve' | 'build';
 type Enforce = 'pre' | 'post' | undefined;
 
-export const ssrBuildVitePlugin = (config: SsrBuildConfig, pluginOverride?: any) => {
+export const ssrBuildVitePlugin = (config: SsrBuildConfig, pluginEvent?: string, pluginOverride?: any) => {
+  pluginEvent ||= 'writeBundle';
   return {
     name: 'ssr-build',
     apply: 'build' as Apply,
     enforce: 'post' as Enforce,
-    writeBundle: async (opts: any, bundle: any) => {
+    [pluginEvent]: async () => {
       const ssrBuild = new SsrBuild(config);
       await ssrBuild.start();
     },
