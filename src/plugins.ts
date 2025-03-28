4de60c3a6dbc1a965caa74b344673959a1f7d2b6
Tpessia <thiago.pessia@gmail.com>
2025-03-27 22:47:05 -0300

    Fix Vite apply and enforce types

--- src/plugins.ts
+++ src/plugins.ts
@@ -4,8 +4,8 @@ import { SsrBuildConfig } from './types';
 export const ssrBuildVitePlugin = (config: SsrBuildConfig) => {
   return {
     name: 'ssr-build',
-    apply: 'build',
-    enforce: 'post',
+    apply: 'build' as 'build',
+    enforce: 'post' as 'post',
     writeBundle: async (opts: any, bundle: any) => {
       const ssrBuild = new SsrBuild(config);
       await ssrBuild.start();
